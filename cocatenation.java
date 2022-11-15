261fc1d16bf179fe3019cc8b52ad0fb556f3f9df
Harsh0019 <118005798+Harsh0019@users.noreply.github.com>
2022-11-15 21:32:05 +0530

    Add files via upload

---
+++ cocatenation.java
@@ -0,0 +1,10 @@
+package string;
+
+public class cocatenation {
+    public static void main(String[] args) {
+        String firstname ="Harsh";
+        String lastname ="Gautam";
+        String fullname = first name + last name;
+        System.out.println(full name);
+    }
+}
